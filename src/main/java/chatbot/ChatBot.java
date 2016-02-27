2e28875da98509f2801770712585c665dab4ffca
Brian Murphy <brianmurphy@gatech.edu>
2016-02-27 17:49:44 -0500

    adding end of chat callback stubs

--- src/main/java/chatbot/ChatBot.java
+++ src/main/java/chatbot/ChatBot.java
@@ -20,12 +20,15 @@ import java.util.regex.Pattern;
 
 public class ChatBot implements Runnable
 {
-	
+
+	public volatile boolean convFound=false;
 	int portNumber;
-	BufferedWriter processOutput;
-	BufferedReader stdInput;
-	Process chatBotProcess;
-	
+	BufferedWriter processOutput=null;
+	BufferedReader stdInput=null;
+	Process chatBotProcess=null;
+
+	FirebaseDataConnection connection;
+
 	private static final double minimumResponseTime=4000;
 	private static final double responseTimeSDev=3000;
 	private static final double meanResponseTime=7000;
@@ -37,38 +40,73 @@ public class ChatBot implements Runnable
 		cb.getResponse("Have you been to the zoo?");
 	}
 	*/
-	public ChatBot(int portNumber)
+	public ChatBot(FirebaseDataConnection connection)
 	{
 		this.portNumber=portNumber;
-		try 
+		this.connection=connection;
+
+		try
 		{
 			chatBotProcess=Runtime.getRuntime().exec("cmd /k cd C:\\Users\\C\\Desktop\\charliebot\\");
-			processOutput=new BufferedWriter(new OutputStreamWriter(chatBotProcess.getOutputStream())); 
-			
+			processOutput=new BufferedWriter(new OutputStreamWriter(chatBotProcess.getOutputStream()));
+
 			processOutput.write("cmd /k run.bat"); //Start chatbot "Charlie"
 			processOutput.newLine();
 			processOutput.flush();
-			
+
 			stdInput=new BufferedReader(new InputStreamReader(chatBotProcess.getInputStream()));
-			Thread.sleep(10000);
-			while (stdInput.ready()) 
+			//Thread.sleep(10000);
+			while (stdInput.ready())
 			{
 				stdInput.read();
-	        }
-		} 
-		catch (IOException | InterruptedException e) 
+			}
+
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private void reset()
+	{
+		try
+		{
+			processOutput.write(""+(char)((byte)3)); //Start chatbot "Charlie"
+			processOutput.newLine();
+			processOutput.flush();
+
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			processOutput.write("Y"); //Start chatbot "Charlie"
+			processOutput.newLine();
+			processOutput.flush();
+
+			processOutput.write("Y"); //Start chatbot "Charlie"
+			processOutput.newLine();
+			processOutput.flush();
+
+			processOutput.write("cmd /k run.bat"); //Start chatbot "Charlie"
+			processOutput.newLine();
+			processOutput.flush();
+		}
+		catch (IOException e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
-	public void run() 
+	public void run()
 	{
-
-		FirebaseDataConnection connection = new FirebaseDataConnection();
-
 		connection.lookForPartner((conversation -> {
 			conversation.listenForMessages((message, pConversation) -> {
 				try {
@@ -102,37 +140,38 @@ public class ChatBot implements Runnable
 //		}
 //
 	}
-	
+
 	private String getResponse(String query) throws IOException, InterruptedException
 	{
+		convFound=true;
 		processOutput.write(query);
 		processOutput.newLine();
 		processOutput.flush();
-		
+
 		responseTime();
-		
+
 		String response="";
-		while (stdInput.ready()) 
+		while (stdInput.ready())
 		{
 			response+=(char)stdInput.read();
-        }
-		
+		}
+
 		int startIndex=response.indexOf("] Charlie> ")+"] Charlie> ".length();
 		int endIndex=response.indexOf("[", startIndex)-2;
-		
+
 		response=response.substring(startIndex, endIndex);
-		
+
 		response=textingGrammar(response);
-		
+
 		System.out.println(response);
 		return response;
 	}
-	
+
 	private void responseTime() throws InterruptedException
 	{
 		Thread.sleep((long)Math.round(Math.max(new Random().nextGaussian()*responseTimeSDev+meanResponseTime, minimumResponseTime)));
 	}
-	
+
 	private String textingGrammar(String proper)
 	{
 		double decapitalizeChance=0.6;
@@ -140,53 +179,53 @@ public class ChatBot implements Runnable
 		double endWithlolChance=0.1;
 		double endWithSmileyFaceChance=0.05;
 		double spellingMistakeChance=0.01;
-		
+
 		for(int index=0; index<proper.length(); index++)
 		{
 			//decapitalize
 			if(proper.charAt(index)>=65 && proper.charAt(index)<=90
-					&& Math.random()<decapitalizeChance)
+					   && Math.random()<decapitalizeChance)
 			{
 				proper=setChar(proper, proper.charAt(index)+32, index);
 			}
-			
+
 			//removePunctuation
 			if((proper.charAt(index)==54 || proper.charAt(index)==56)
-					&& Math.random()<removePunctuationChance)
+					   && Math.random()<removePunctuationChance)
 			{
 				proper=removeChar(proper, index);
 				index--;
 			}
-			
+
 			//spelling mistakes
 			if(Math.random()<spellingMistakeChance)
 			{
 				proper=setChar(proper, 97+(int)(26*Math.random()), index);
 			}
-			
+
 		}
-		
+
 		//end with lol
 		if(Math.random()<endWithlolChance)
 		{
 			proper+=" lol";
 		}
-		
+
 		//end with :)
 		if(Math.random()<endWithSmileyFaceChance)
 		{
 			proper+=" :)";
 		}
-		
+
 		return proper;
 	}
-	
+
 	private String setChar(String string, int charToSet, int index)
 	{
 		string=string.substring(0, index)+((char)charToSet)+string.substring(index+1);
 		return string;
 	}
-	
+
 	private String removeChar(String string, int index)
 	{
 		string=string.substring(0, index)+string.substring(index+1);
--- src/main/java/chatbot/ChatBotManager.java
+++ src/main/java/chatbot/ChatBotManager.java
@@ -1,22 +1,42 @@
 package chatbot;
 
-public class ChatBotManager 
+import java.io.BufferedWriter;
+import java.net.ServerSocket;
+import java.util.Hashtable;
+import java.util.Random;
+
+import network.FirebaseDataConnection;
+
+public class ChatBotManager
 {
-	
-	private static final int startPort=28250;
-	
+
+	FirebaseDataConnection connection;
+
 	public static void main(String[] args)
 	{
 		new ChatBotManager(1);
 	}
-	
+
 	public ChatBotManager(int numberBots)
 	{
+		connection=new FirebaseDataConnection();
 		for(int created=0; created<numberBots; created++)
 		{
-			ChatBot newChatBot = new ChatBot(startPort+created);
+			ChatBot newChatBot=new ChatBot(connection);
 			new Thread(newChatBot).run();
+			/*
+			while(!newChatBot.convFound)
+			{
+				try
+				{
+					Thread.sleep(50);
+				} catch (InterruptedException e)
+				{
+					e.printStackTrace();
+				}
+			}
+			*/
 		}
 	}
-	
+
 }
